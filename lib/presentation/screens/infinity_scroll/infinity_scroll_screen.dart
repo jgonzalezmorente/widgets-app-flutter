48f8e25f3e690c399837e1a5236a78c27ca67dde
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-05-04 17:27:03 +0200

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/infinity_scroll/infinity_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfinityScrollScreen extends StatelessWidget {
+  static const name = 'infinity_scroll_screen';
+
+  const InfinityScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
