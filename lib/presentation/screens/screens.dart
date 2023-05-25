4fc7677ac3ca174d59606f618a48e2327113843a
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-05-25 10:19:55 +0200

    infinite scroll finish

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -3,7 +3,7 @@ export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial_scree
 export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
-export 'package:widgets_app/presentation/screens/infinity_scroll/infinity_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
 export 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
 export 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
 export 'package:widgets_app/presentation/screens/ui_controls/ui_controls_screen.dart';
